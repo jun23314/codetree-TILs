e4aa49b55de5e889eabd90472b6df42a9fc174fa
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-01-12 10:55:07 +0900

    Title: 트리 노드 제거, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240112/트리 노드 제거/remove-tree-node.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+vector<int> arr[51];
+vector<int>::iterator it;
+bool deleted[51] = {0, };
+int cnt = 0;
+
+void dfs(int node){
+    if (deleted[node]) return;
+
+    bool leaf = 1;
+
+    for(it = arr[node].begin(); it != arr[node].end(); ++it){
+        int new_node = *it;
+        if(deleted[new_node]) continue;
+        dfs(new_node);
+        leaf = 0;
+    }
+    if(leaf) cnt++;
+}
+int main() {
+    int n;
+    cin >> n;
+
+    int root = 0;
+    for(int i = 0; i < n; ++i){
+        int a;
+        cin >> a;
+        
+        if(a == -1) root = a;
+        else arr[a].push_back(i);
+    }
+
+    int del;
+    cin >> del;
+
+
+    deleted[del] = 1;
+
+    dfs(root);
+
+    cout << cnt;
+    return 0;
+}
