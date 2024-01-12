55b50f307bc1f7742d7f15386a829ff177d530a7
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-01-12 10:56:37 +0900

    Title: 트리 노드 제거, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/트리 노드 제거/remove-tree-node.cpp
+++ 240112/트리 노드 제거/remove-tree-node.cpp
@@ -1,47 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 
-using namespace std; 
+#define MAX_N 50
 
-vector<int> arr[51];
-vector<int>::iterator it;
-bool deleted[51] = {0, };
-int cnt = 0;
+using namespace std;
 
-void dfs(int node){
-    if (deleted[node]) return;
+// 변수 선언:
+int n;
+int ans;
+int root;
+int par[MAX_N];
+vector<int> edges[MAX_N];
+bool is_deleted[MAX_N];
 
-    bool leaf = 1;
+// DFS를 통해 x 아래의 리프노드의 개수를 찾아줍니다.
+void DFS(int x) {
+    // (루트 노드가 삭제되었을 때) 삭제된 노드는 스킵합니다.
+    if(is_deleted[x])
+        return;
 
-    for(it = arr[node].begin(); it != arr[node].end(); ++it){
-        int new_node = *it;
-        if(deleted[new_node]) continue;
-        dfs(new_node);
-        leaf = 0;
-    }
-    if(leaf) cnt++;
-}
-int main() {
-    int n;
-    cin >> n;
+    // x번 노드가 리프노드인지 판단합니다. 자신의 자식 노드가
+    // 하나라도 남아 있을 경우 x번 노드는 리프노드가 아닙니다.
+    bool is_leaf = true;
 
-    int root = 0;
-    for(int i = 0; i < n; ++i){
-        int a;
-        cin >> a;
+    for(int i = 0; i < edges[x].size(); i++) {
+        int y = edges[x][i];
         
-        if(a == -1) root = a;
-        else arr[a].push_back(i);
+        // 삭제된 노드는 스킵합니다.
+        if(is_deleted[y]) 
+            continue;
+        
+        DFS(y);
+        is_leaf = false;
     }
 
-    int del;
-    cin >> del;
+    // 자신 노드가 리프 노드일 경우 답을 갱신합니다.
+    if(is_leaf)
+        ans++;
+}
 
+int main() {
+    // 입력:
+    cin >> n;
+    // n개의 간선 정보를 입력받습니다.
+    for(int i = 0; i < n; i++) {
+        int x, y;
+        cin >> par[i];
+        x = i;
+        y = par[i];
 
-    deleted[del] = 1;
+        // -1이 입력된 경우 루트 노드로 지정합니다.
+        if(y == -1) {
+            root = x;
+            continue;
+        }
+        
+        // 간선 정보를 인접리스트에 넣어줍니다.
+        edges[y].push_back(x);
+    }
 
-    dfs(root);
+    // 노드를 제거합니다.
+    int del_node;
+    cin >> del_node;
 
-    cout << cnt;
+    // 노드를 제거하는 방법에는 간선을 직접 끊어주는 등 여러 방법이 있습니다.
+    // 여기서는 삭제되었는지 여부를 배열로 관리합니다.
+    is_deleted[del_node] = true;
+
+    // root 정점으로부터 리프노드의 개수를 찾습니다.
+    DFS(root);
+
+    cout << ans;
     return 0;
 }
