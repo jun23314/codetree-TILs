16b6a8f2233d2eeb0b7dbb25c68ab2373e026bc6
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-01-25 11:01:27 +0900

    Title: 경로의 적합성 판단 2, Time: 103ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, k;
+int uf[100001];
+
+int find(int x){
+    if(uf[x] == x) return x;
+    int root = find(uf[x]);
+    uf[x] = root;
+    return root;
+}
+
+void unions(int x, int y){
+    int a = find(x);
+    int b = find(b);
+    uf[a] = b;
+}
+
+int main() {
+    cin >> n >> m >> k;
+    for(int i = 0; i < m; ++i){
+        int a, b;
+        cin >> a >> b;
+        unions(a, b);
+    }
+
+    if (k == 1) {
+        cout << 1;
+        return 0;
+    }
+    int a;
+    cin >> a;
+    for(int i = 0; i < k; i++){
+        int b;
+        cin >> b;
+        if(find(a) != find(b)){
+            cout << 0;
+            return 0;
+        }
+        cin >> a;
+    }
+    cout << 1;
+    return 0;
+}
