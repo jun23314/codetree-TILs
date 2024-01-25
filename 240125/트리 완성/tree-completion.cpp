10a2984049c7fff7619a0957346eb51697e9763f
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-01-25 15:52:46 +0900

    Title: 트리 완성, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/트리 완성/tree-completion.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int uf[100001];
+int size[100001] = {0, };
+int cnt = 0;
+
+int find(int x){
+    if(uf[x] == x) return x;
+    int root = find(uf[x]);
+    uf[x] = root;
+    return root;
+}
+
+void unions(int x, int y){
+    int a = find(x);
+    int b = find(y);
+    if (a != b) {
+        uf[a] = b;
+        size[b] += size[a];
+        size[a] += size[b];
+    }
+    else{
+        cnt++;
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; ++i){
+        uf[i] = i;
+        size[i] = 1;
+    }
+
+    for(int i = 0; i < m; ++i){
+        int a, b;
+        cin >> a >> b;
+        unions(a, b);
+    }
+
+    int max = -1;
+    for(int i = 1; i <= n; ++i){
+        if(size[i] > max) max = size[i];
+    }
+    
+    cnt += (n-max);
+
+    cout << cnt;
+    return 0;
+}
