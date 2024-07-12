ad81b3e6a6becab446b9fc3bfd6d899ef4c2c459
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-07-12 16:05:58 +0900

    Title: xor 결과 최대 만들기, Time: 1096ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240712/xor 결과 최대 만들기/max-of-xor.cpp
+++ 240712/xor 결과 최대 만들기/max-of-xor.cpp
@@ -8,22 +8,16 @@ int num[21];
 int max_val = 0;
 vector<int> v;
 
-void checkXor(){
-    int result = 0;
-    for(auto i: v) result = result^i;
-    if(result > max_val) max_val = result;
-}
-
 void choose(int curr_idx, int curr_val, int result){
     if(curr_idx == m + 1){
+
         if(result > max_val) max_val = result;
         return ;
     }
     
     for(int i = curr_val; i < n; ++i){
         v.push_back(num[i]);
-        result = result^num[i];
-        choose(curr_idx+1, curr_val+1, result);
+        choose(curr_idx+1, curr_val+1, result^num[i]);
         v.pop_back();
     }
 }
