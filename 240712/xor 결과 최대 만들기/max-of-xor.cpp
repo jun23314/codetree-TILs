5b394697871c5522c663df72a948d7be578d36f7
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-07-12 15:34:37 +0900

    Title: xor 결과 최대 만들기, Time: 1042ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240712/xor 결과 최대 만들기/max-of-xor.cpp
+++ 240712/xor 결과 최대 만들기/max-of-xor.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 int n, m;
 int num[21];
 int max_val = 0;
-vector<int> v;
+unordered_set<int> v;
 
 void checkXor(){
     int result = 0;
-    for(auto i: v)result = result^i;
+    for(auto i: v) result = result^i;
     if(result > max_val) max_val = result;
 }
 
@@ -19,11 +19,14 @@ void choose(int curr_idx){
         checkXor();
         return ;
     }
-
-    for(auto i: num){
-        v.push_back(i);
-        choose(curr_idx+1);
-        v.pop_back();
+    
+    for(int i = 0; i < n; ++i){
+        if(v.find(num[i]) == v.end()){
+            v.insert(num[i]);
+            choose(curr_idx+1);
+            v.erase(num[i]);
+        }
+        
     }
 }
 int main() {
@@ -32,6 +35,7 @@ int main() {
     for(int i = 0; i < n; ++i){
         scanf("%d", &num[i]);
     }
+
     choose(1);
     printf("%d", max_val);
     return 0;
