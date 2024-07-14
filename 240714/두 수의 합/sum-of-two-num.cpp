3d89e0e862fc41a649ddebacc26a5bbc5f2c32d8
jun23314 <116951160+jun23314@users.noreply.github.com>
2024-07-14 15:39:27 +0900

    Title: 두 수의 합, Time: 13ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240714/두 수의 합/sum-of-two-num.cpp
+++ 240714/두 수의 합/sum-of-two-num.cpp
@@ -8,6 +8,7 @@ int cnt = 0;
 unordered_map<int, int> m;
 int arr[100001] = {0, 0, 1, };
 int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n >> k;
 
     for(int i = 0; i < n; ++i){
